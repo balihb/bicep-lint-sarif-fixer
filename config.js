9fd055a91e72b29e3303f091bf1b5ad1a9730464
balihb <balihb@gmail.com>
2024-12-01 00:19:45 +0100

    ci: renovate config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// https://docs.renovatebot.com/configuration-options/
+
+module.exports = {
+    onboarding: true,
+    platform: "github",
+    repositories: ["balihb/bicep-lint-sarif-fixer"],
+};
