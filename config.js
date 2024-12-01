b5c9ae74f467d7fcbefdf87e6590296819db9813
balihb <balihb@gmail.com>
2024-12-01 09:22:46 +0100

    ci: json5 renovate config

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
 // https://docs.renovatebot.com/configuration-options/
 
 module.exports = {
+    configMigration: true,
+    dependencyDashboardOSVVulnerabilitySummary: 'all',
+    fetchChangeLogs: 'on',
     onboarding: true,
+    onboardingConfigFileName: "renovate.json5",
+    osvVulnerabilityAlerts: true,
     platform: "github",
     repositories: ["balihb/bicep-lint-sarif-fixer"],
 };
